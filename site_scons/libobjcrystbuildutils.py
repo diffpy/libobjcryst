9ffd165b72606b966ef89c15ae0dae39a42e4193
Pavol Juhas <pavol.juhas@gmail.com>
2015-10-01 18:05:30 -0400

    Add importable gitinfo function for scons.
    Python function for extracting version data.  Adopted from
    libdiffpy with pre-release versioning conformant to PEP 0440.
    Pre-release versions are now formatted as 2015.1a0.devN,
    post-releases as 2015.1.N.

---
+++ site_scons/libobjcrystbuildutils.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+
+'''Utility functions used by scons and sphinx for version extraction.
+'''
+
+import os
+
+MYDIR = os.path.dirname(os.path.abspath(__file__))
+
+def gitinfo():
+    'Extract dictionary of version data from git records.'
+    import re
+    from subprocess import Popen, PIPE
+    global _cached_gitinfo
+    if _cached_gitinfo is not None:  return _cached_gitinfo
+    rv = _cached_gitinfo = {}
+    nullfile = open(os.devnull, 'w')
+    kw = dict(stdout=PIPE, stderr=nullfile, cwd=MYDIR)
+    proc = Popen(['git', 'describe', '--match=v[[:digit:]]*'], **kw)
+    desc = proc.stdout.read()
+    if proc.wait():  return gitinfo()
+    proc = Popen(['git', 'log', '-1', '--format=%H %ai'], **kw)
+    glog = proc.stdout.read()
+    words = desc.strip().split('-')
+    vtag = words[0].lstrip('v')
+    vnum = int((words[1:2] or ['0'])[0])
+    rv['commit'], rv['date'] = glog.strip().split(None, 1)
+    mx = re.match(r'^(\d+)\.(\d+)((?:[ab]|rc)\d*)?', vtag)
+    rv['major'] = int(mx.group(1))
+    rv['minor'] = int(mx.group(2))
+    rv['prerelease'] = mx.group(3)
+    rv['number'] = vnum
+    rv['version'] = vtag
+    numsep = rv['prerelease'] and '.dev' or '.'
+    if vnum:
+        rv['version'] += numsep + str(vnum)
+    _cached_gitinfo = rv
+    return gitinfo()
+
+_cached_gitinfo = None
