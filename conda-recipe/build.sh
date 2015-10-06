956e4248fc4d65370e717c6a280ba4ae89d4f7e9
Pavol Juhas <pavol.juhas@gmail.com>
2015-10-06 11:52:10 -0400

    Use the current git clone in conda recipe.
    Extract conda-package version from ObjCryst/version.h.

--- conda-recipe/build.sh
+++ conda-recipe/build.sh
@@ -5,3 +5,6 @@ MYNCPU=$(( (CPU_COUNT > 4) ? 4 : CPU_COUNT ))
 
 scons -j $MYNCPU
 scons install prefix=$PREFIX
+
+grep '^#define LIBOBJCRYST_VERSION_STR' < "${PREFIX}/include/ObjCryst/version.h" |
+    cut -d '"' -f 2 > __conda_version__.txt
