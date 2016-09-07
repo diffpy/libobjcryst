e6c4ff618194670aa11ef5918ce26587f313163f
Pavol Juhas <pavol.juhas@gmail.com>
2016-09-07 15:50:11 -0400

    Stop using deprecated __conda_version__.txt file.
    Add dummy setup.py file and use the jinja template function to get the
    version string.

--- conda-recipe/build.sh
+++ conda-recipe/build.sh
@@ -5,6 +5,3 @@ MYNCPU=$(( (CPU_COUNT > 8) ? 8 : CPU_COUNT ))
 
 scons -j $MYNCPU
 scons install prefix=$PREFIX
-
-grep '^#define LIBOBJCRYST_VERSION_STR' < "${PREFIX}/include/ObjCryst/version.h" |
-    cut -d '"' -f 2 > __conda_version__.txt
