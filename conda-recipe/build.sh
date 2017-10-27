93ad2ce4223148a68f1591592c317c18edcbd78f
Pavol Juhas <pavol.juhas@gmail.com>
2017-10-27 12:24:06 -0400

    BLD: build with conda c++ toolchain.

--- conda-recipe/build.sh
+++ conda-recipe/build.sh
@@ -3,5 +3,8 @@
 export CPATH="$PREFIX/include:$CPATH"
 MYNCPU=$(( (CPU_COUNT > 8) ? 8 : CPU_COUNT ))
 
+# Apply sconscript.local customizations.
+cp ${RECIPE_DIR}/sconscript.local ./
+
 scons -j $MYNCPU
 scons install prefix=$PREFIX
